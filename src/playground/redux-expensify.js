8330ed119ac7dabe7005ef8f06cb27be08d1b0dd
Christopher Ley <info@christopherley.com>
2017-11-14 11:13:04 -0300

    explored initial redux methodology

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -151,6 +151,12 @@ const getVisableExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if (sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1
+        } else if (sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1
+        }
     });
 };
 
