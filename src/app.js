4f67e4b90d5c8fafc91aca90b43e41c3e0cfb291
Christopher Ley <info@christopherley.com>
2017-11-14 11:41:02 -0300

    initial setup of redux infrustructure

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import getVisableExpenses from './selectors/expenses';
+import { addExpense, removeExpense, editExpense } from './actions/expenses';
+import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
 import 'normalize.css/normalize.css';
 import "./styles/styles.scss";
+
+const store = configureStore();
+store.subscribe(() => {
+    const state = store.getState();
+    const visableExpenses = getVisableExpenses(state.expenses, state.filters);
+    console.log(visableExpenses);
+});
+
+const expenseOne = store.dispatch(addExpense({ 
+    description: 'Water bill', amount: 1500 
+}));
+const expenseTwo = store.dispatch(addExpense({ 
+    description: 'Gas bill', amount: 3000
+}));
+store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/reducers/expenses.js
@@ -0,0 +1,27 @@
+// Expenses Reducer
+const expensesReducerDefaultState = [];
+
+export default (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state, // this is the array spread operator
+                action.expense
+            ];
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => action.id !== id)
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return { // this is the object spread operator
+                        ...expense,
+                        ...action.updates
+                    }
+                } else {
+                    return expense;
+                }
+            });
+        default: 
+            return state;
+    }
+};
