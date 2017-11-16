ad729c2d7e1241b4cbe8ff1d61f32cf7025749a8
Christopher Ley <info@christopherley.com>
2017-11-16 11:50:44 -0300

    started testing forms

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import { addExpense, removeExpense, editExpense } from './actions/expenses';
 import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from './actions/filters';
 import 'normalize.css/normalize.css';
 import "./styles/styles.scss";
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 // store.subscribe(() => {
